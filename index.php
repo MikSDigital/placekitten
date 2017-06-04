886f93a6cba9c1c9e888f6998e3dc882006f85be
MikSDigital <mixa.ru.ru@gmail.com>
2017-06-04 23:02:48 +0300

    Image processing added

--- index.php
+++ index.php
@@ -15,13 +15,22 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(),[
     ]
 ]);
 
+$app->register(new App\Providers\ImageServiceProvider);
+
 $app->get('/', function (Request $request, Silex\Application $app){
 
     $image = $app['db']->fetchAssoc("SELECT * FROM images WHERE id=2");
 
-    return $app->json($image);
+    $placeholder = $app['image']
+        ->make(__DIR__ . '/images/docker.png')
+        ->fit(800,600)
+        ->greyscale()
+        ->response('png');
 
-    return new Response($image,200);
+    return new Response($placeholder,200,[
+        'Content-Type' => 'image/png'
+    ]);
+
 });
 
 $app->run();
