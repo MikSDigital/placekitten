b8bac5b91d8de22d93afc3d9b55b216377c8d0dd
MikSDigital <mixa.ru.ru@gmail.com>
2017-06-04 23:46:50 +0300

    Validation

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\{Response, Request};
 
+use Illuminate\Validation;
+use Illuminate\Filesystem;
+use Illuminate\Translation;
+
 $app = new Silex\Application();
 
 $app['debug'] = true;
@@ -18,6 +22,37 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(),[
 $app->register(new App\Providers\ImageServiceProvider);
 
 $app->get('/', function (Request $request, Silex\Application $app){
+
+// https://stackoverflow.com/questions/28573889/illuminate-validator-in-stand-alone-non-laravel-application
+
+    $filesystem = new Filesystem\Filesystem();
+    $fileloader = new Translation\FileLoader($filesystem,'');
+    $translator = new Translation\Translator($fileloader,'en_US');
+    $factory = new Validation\Factory($translator);
+
+
+
+
+    $messages = [
+        'required' => 'The :attribute field is required.',
+    ];
+
+    $dataToValidate = ['title' => 'Some title'];
+    $rules = [
+        'title' => 'required',
+        'body' => 'required'
+    ];
+
+    $validator = $factory->make($dataToValidate, $rules, $messages);
+
+    if($validator->fails()){
+        $errors = $validator->errors();
+        foreach($errors->all() as $message){
+//            var_dump($message);
+        }
+    }
+
+
 
     $image = $app['db']->fetchAssoc("SELECT * FROM images WHERE id=2");
 
